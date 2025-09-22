db9e4c62a29c6bc42547d42b8e39c12447ecf71f
assykot48 <assykot48@gmail.com>
2025-09-22 16:40:16 +0500

    убрала запись в лог из обработчика

--- deploy.php
+++ deploy.php
@@ -1,17 +1,17 @@
 <?php
 $secret = '@WowE_(Cj7';
-$log_file = '/var/www/u3265633/data/www/10domov.ru/deploy-webhook.log';
+//$log_file = '/var/www/u3265633/data/www/10domov.ru/deploy-webhook.log';
 $github_signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
-file_put_contents($log_file, "GitHub Signature: " . $github_signature . "\n", FILE_APPEND);
+//file_put_contents($log_file, "GitHub Signature: " . $github_signature . "\n", FILE_APPEND);
 
 // Получаем данные от GitHub
 $payload = json_decode(file_get_contents('php://input'), true);
 if (!verifyGitHubSignature($payload, $github_signature, $secret)) {
-    file_put_contents($log_file, "❌ Signature verification failed\n", FILE_APPEND);
+    //file_put_contents($log_file, "❌ Signature verification failed\n", FILE_APPEND);
     http_response_code(403);
     exit('Forbidden');
 }
-file_put_contents($log_file, date('Y-m-d H:i:s') . print_r($payload, true) . "\n", FILE_APPEND);
+//file_put_contents($log_file, date('Y-m-d H:i:s') . print_r($payload, true) . "\n", FILE_APPEND);
 
 // Обрабатываем только пуши в master и мердж-реквесты
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] === 'pull_request') {
@@ -30,7 +30,7 @@ function verifyGitHubSignature($payload, $github_signature, $secret) {
         return false;
     }
     $expected_signature = 'sha256=' . hash_hmac('sha256', $payload, $secret);
-    
+
     return hash_equals($expected_signature, $github_signature);
 }
 ?>

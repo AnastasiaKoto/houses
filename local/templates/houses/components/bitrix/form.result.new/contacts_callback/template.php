4f14c53d79dba1614e9d31a1c2c8caf5a02487cb
assykot48 <assykot48@gmail.com>
2025-10-23 17:09:10 +0500

    fix contact form

--- local/templates/houses/components/bitrix/form.result.new/contacts_callback/template.php
+++ local/templates/houses/components/bitrix/form.result.new/contacts_callback/template.php
@@ -9,42 +9,43 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
  */
 
 ?>
-<? if ($arResult["isFormNote"] == "Y") { 
-	if(isset($_REQUEST['RESULT_ID'])) {
-		$resultId = intval($_REQUEST['RESULT_ID']);
-		$fieldId = 0;
-		foreach($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
-			if($FIELD_SID == 'PHONE') {
-				$fieldId = $arQuestion['STRUCTURE'][0]['ID'];
-				break;
+<div class="contacts-form" data-move-target=".contacts-map__frame" data-move-break="992">
+	<? if ($arResult["isFormNote"] == "Y") {
+		if (isset($_REQUEST['RESULT_ID'])) {
+			$resultId = intval($_REQUEST['RESULT_ID']);
+			$fieldId = 0;
+			foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
+				if ($FIELD_SID == 'PHONE') {
+					$fieldId = $arQuestion['STRUCTURE'][0]['ID'];
+					break;
+				}
 			}
+			$userPhone = getUserPhone($resultId, $fieldId);
 		}
-		$userPhone = getUserPhone($resultId, $fieldId);
-	}
-?>
-	<div class="thx-inner">
-		<div class="section-title">
-		Ваша заявка успешно отправлена!
-		</div>
-		<? if (isset($userPhone) && !empty($userPhone)): ?>
-			<div class="question-form__form-subtitle">
-			Наш менеджер свяжется с вами по номеру:
+		?>
+		<div class="thx-inner">
+			<div class="section-title">
+				Ваша заявка успешно отправлена!
 			</div>
-		
-            <span class="question-form__form-phone">
-                <?= htmlspecialcharsbx($userPhone) ?>
-			</span>
-		<? endif; ?>
-		<a href="javascript:void(0)" onclick="window.location.reload();" class="question-form__form-close ask-btn">
-		Закрыть
-		</a>
-	</div>
-	<script>
-		document.querySelector('.main_section-title').style.display = 'none';
-		document.querySelector('.main_question-form__form-subtitle').style.display = 'none';
-	</script>
-<? } elseif ($arResult["isFormNote"] != "Y") { ?>
-	<div class="contacts-form" data-move-target=".contacts-map__frame" data-move-break="992">
+			<? if (isset($userPhone) && !empty($userPhone)): ?>
+				<div class="question-form__form-subtitle">
+					Наш менеджер свяжется с вами по номеру:
+				</div>
+
+				<span class="question-form__form-phone">
+					<?= htmlspecialcharsbx($userPhone) ?>
+				</span>
+			<? endif; ?>
+			<a href="javascript:void(0)" onclick="window.location.reload();" class="question-form__form-close ask-btn">
+				Закрыть
+			</a>
+		</div>
+		<script>
+			document.querySelector('.main_section-title').style.display = 'none';
+			document.querySelector('.main_question-form__form-subtitle').style.display = 'none';
+		</script>
+	<? } elseif ($arResult["isFormNote"] != "Y") { ?>
+
 		<div class="contacts-form__title">
 			<?= $arResult['arForm']['NAME']; ?>
 		</div>
@@ -52,15 +53,15 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 			<?= $arResult['arForm']['DESCRIPTION']; ?>
 		</div>
 		<?= $arResult["FORM_HEADER"] ?>
-			<? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) { ?>
-				<? if($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] != "checkbox") { 
-					
-					if($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] == "text") {
-				?>
+		<? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) { ?>
+			<? if ($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] != "checkbox") {
+
+				if ($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] == "text") {
+					?>
 					<div class="float-input input-wrapper">
 						<?= recreateTextField($FIELD_SID, $arQuestion, 'text'); ?>
 					</div>
-					<? } elseif($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] == "hidden") { ?>
+				<? } elseif ($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] == "hidden") { ?>
 					<div class="custom-select input-wrapper" data-placeholder="<?= $arQuestion['CAPTION']; ?>">
 						<div class="custom-select__trigger">
 							<span class="custom-select__value"></span>
@@ -83,60 +84,59 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 						</ul>
 						<?= $arQuestion["HTML_CODE"]; ?>
 					</div>
-					<? } ?>
 				<? } ?>
 			<? } ?>
-			<? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) { 
-				if($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] == "checkbox") { 
-			?>
-			<label class="custom-checkbox-black input-wrapper">
-				<?= recreateTextField($FIELD_SID, $arQuestion, $arQuestion["STRUCTURE"][0]["FIELD_TYPE"]); ?>
-				<? // $arQuestion["HTML_CODE"]; ?>
-				<span class="checkmark">
-					<svg width="12" height="10" viewBox="0 0 12 10" fill="none"
-						xmlns="http://www.w3.org/2000/svg">
-						<path d="M1.3335 5.66406L4.00016 8.33073L10.6668 1.66406" stroke="white"
-							stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					</svg>
-				</span>
-				<span class="checkbox-text"><?= $arQuestion['CAPTION']; ?></span>
-			</label>
-			<? } } ?>
-			<button type="submit" class="arrow-btn__dark modal-submit" name="web_form_submit" value="<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == "" ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>">
-				<span>
-					<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == "" ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>
-				</span>
-				<div class="icon">
-					<svg width="13" height="13" viewBox="0 0 13 13" fill="none"
-						xmlns="http://www.w3.org/2000/svg">
-						<path
-							d="M11.4167 6.5L6.41667 11.5M1 6.5H11.4167H1ZM11.4167 6.5L6.41667 1.5L11.4167 6.5Z"
-							stroke="#2E2F33" stroke-width="1.5" stroke-linecap="round"
-							stroke-linejoin="round" />
-					</svg>
-				</div>
-			</button>
+		<? } ?>
+		<? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
+			if ($arQuestion["STRUCTURE"][0]["FIELD_TYPE"] == "checkbox") {
+				?>
+				<label class="custom-checkbox-black input-wrapper">
+					<?= recreateTextField($FIELD_SID, $arQuestion, $arQuestion["STRUCTURE"][0]["FIELD_TYPE"]); ?>
+					<? // $arQuestion["HTML_CODE"]; ?>
+					<span class="checkmark">
+						<svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M1.3335 5.66406L4.00016 8.33073L10.6668 1.66406" stroke="white" stroke-width="1.5"
+								stroke-linecap="round" stroke-linejoin="round" />
+						</svg>
+					</span>
+					<span class="checkbox-text"><?= $arQuestion['CAPTION']; ?></span>
+				</label>
+			<? }
+		} ?>
+		<button type="submit" class="arrow-btn__dark modal-submit" name="web_form_submit"
+			value="<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == "" ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>">
+			<span>
+				<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == "" ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>
+			</span>
+			<div class="icon">
+				<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M11.4167 6.5L6.41667 11.5M1 6.5H11.4167H1ZM11.4167 6.5L6.41667 1.5L11.4167 6.5Z"
+						stroke="#2E2F33" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+				</svg>
+			</div>
+		</button>
 		<?= $arResult["FORM_FOOTER"]; ?>
+
+		<? if ($arResult["isFormErrors"] == "Y") {
+			$fields = [];
+			foreach ($arResult['FORM_ERRORS'] as $key => $value) {
+				$fields[] = $key;
+			}
+			?>
+			<script>
+				(() => {
+					const fields = <?= json_encode($fields, JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) ?>;
+					const form = document.querySelector('.contacts-form form[name="<?= $arResult['arForm']['SID']; ?>"]');
+					if (Array.isArray(fields) && fields.length > 0) {
+						fields.forEach(field => {
+							let parent = form.querySelector(`#${field}`).closest('.input-wrapper');
+							if (!parent.classList.contains('error')) {
+								parent.classList.add('error');
+							}
+						});
+					}
+				})();
+			</script>
+		<? } ?>
 	</div>
-	<?if ($arResult["isFormErrors"] == "Y") { 
-		$fields = [];
-		foreach($arResult['FORM_ERRORS'] as $key => $value) {
-			$fields[] = $key;
-		}
-		?>
-		<script>
-			(() => {
-				const fields = <?= json_encode($fields, JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) ?>;
-				const form = document.querySelector('.contacts-form form[name="<?= $arResult['arForm']['SID']; ?>"]');
-				if (Array.isArray(fields) && fields.length > 0) {
-					fields.forEach(field => {
-						let parent = form.querySelector(`#${field}`).closest('.input-wrapper');
-						if(!parent.classList.contains('error')) {
-							parent.classList.add('error');
-						}
-					});
-				}
-			})();
-		</script>
-	<? } ?>
 <? } ?>

131e25b59720c1bc1c7bcb1bbca94f4eea19fe36
assykot48 <assykot48@gmail.com>
2025-11-01 15:52:58 +0500

    страница технологии

---
+++ local/templates/houses/components/bitrix/news.list/compare/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.compare-tabs__link').forEach(tab => {
+        tab.addEventListener('click', () => {
+            const tabId = tab.dataset.tab;
+
+        
+            document.querySelectorAll('.compare-tabs__link')
+            .forEach(el => el.classList.remove('active'));
+            tab.classList.add('active');
+
+
+            document.querySelectorAll('.compare-tabs__content')
+            .forEach(content => {
+                content.classList.remove('active');
+                if (content.dataset.tabContent === tabId) {
+                content.classList.add('active');
+                }
+            });
+        });
+    });
+})
--- tekhnologii/index.php
+++ tekhnologii/index.php
@@ -3,23 +3,23 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Технологии");
 ?>
 <section class="section">
-    <div class="container">
-        <h1 class="section-title">
-            <? $APPLICATION->ShowTitle(false); ?>
-        </h1>
-        <div class="section-subtitle">
-            <? $APPLICATION->IncludeComponent(
-                "bitrix:main.include",
-                "",
-                array(
-                    "AREA_FILE_SHOW" => "file",
-                    "AREA_FILE_SUFFIX" => "",
-                    "EDIT_TEMPLATE" => "standard.php",
-                    "PATH" => "/include/technology/page_descr.php"
-                )
-            ); ?>
-        </div>
-    </div>
+	<div class="container">
+		<h1 class="section-title">
+			<? $APPLICATION->ShowTitle(false); ?>
+		</h1>
+		<div class="section-subtitle">
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				array(
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "",
+					"EDIT_TEMPLATE" => "standard.php",
+					"PATH" => "/include/technology/page_descr.php"
+				)
+			); ?>
+		</div>
+	</div>
 </section>
 <? $APPLICATION->IncludeComponent(
 	"bitrix:main.include",
@@ -41,41 +41,101 @@ $APPLICATION->SetTitle("Технологии");
 		"PATH" => "/include/general/blocks/unics.php"
 	)
 ); ?>
-<? $APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	array(
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => "/include/technology/compare_block/section_title.php"
-	)
+<section class="section compare">
+	<div class="container">
+		<div class="section-title">
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				array(
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "",
+					"EDIT_TEMPLATE" => "standard.php",
+					"PATH" => "/include/technology/compare_block/section_title.php"
+				)
+			); ?>
+		</div>
+		<div class="section-subtitle">
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				array(
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "",
+					"EDIT_TEMPLATE" => "standard.php",
+					"PATH" => "/include/technology/compare_block/section_descr.php"
+				)
+			); ?>
+		</div>
+		<? $APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"compare", 
+	[
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"AJAX_MODE" => "N",
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_ID" => IB_COMPARE_ID,
+		"NEWS_COUNT" => "20",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => [
+			0 => "ID",
+			1 => "",
+		],
+		"PROPERTY_CODE" => [
+			0 => "",
+			1 => "PICTURES",
+		],
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "N",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "3600",
+		"CACHE_FILTER" => "Y",
+		"CACHE_GROUPS" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => "",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => "",
+		"PAGER_BASE_LINK" => "",
+		"PAGER_PARAMS_NAME" => "arrPager",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"COMPONENT_TEMPLATE" => "compare",
+		"STRICT_SECTION_CHECK" => "N"
+	],
+	false
 ); ?>
-<? $APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	array(
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => "/include/technology/compare_block/section_descr.php"
-	)
-); ?>
-<? $APPLICATION->IncludeComponent(
-"bitrix:highloadblock.list",
-"",
-array(
-	"BLOCK_ID" => HL_COMPARE_ID,	// ID highload блока
-	"CHECK_PERMISSIONS" => "N",	// Проверять права доступа
-	"DETAIL_URL" => "",	// Путь к странице просмотра записи
-	"FILTER_NAME" => "",	// Идентификатор фильтра
-	"PAGEN_ID" => "",	// Идентификатор страницы
-	"ROWS_PER_PAGE" => "",	// Разбить по страницам количеством
-	"SORT_FIELD" => "ID",	// Поле сортировки
-	"SORT_ORDER" => "ASC",	// Направление сортировки
-),
-false
-); ?>
+	</div>
+</section>
 <? $APPLICATION->IncludeComponent(
 	"bitrix:main.include",
 	"",
